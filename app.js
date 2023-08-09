8564a8fb256d8be311fd998468fc41d8d27f0319
Miles Keaveny <mkeaveny@wesleyan.edu>
2023-08-08 20:30:33 -0400

    Much Progress

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
+// app.js
+
+/* const navbar = document.querySelector('#NavBar');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
+
+document.getElementById("test").children[0].className += " load"; */
+
+
+const nav = document.getElementById("nav");
+window.onscroll = function () { 
+    console.log(document.documentElement.scrollTop);
+    if (document.documentElement.scrollTop >= 55 ) {
+        nav.classList.add("nav-colored");
+        nav.classList.remove("nav-transparent");
+    } 
+    else {
+        nav.classList.add("nav-transparent");
+        nav.classList.remove("nav-colored");
+    }
+};
+
+/*const controllLink = document.querySelectorAll(".control");
+
+
+const homeLink = document.querySelector('.control-1');
+const aboutLink = document.querySelector('.control-2');
+const portfolioLink = document.querySelector('.control-4');
+
+homeLink.addEventListener('click', () => {
+    window.location.href = 'splash.html';
+});
+
+aboutLink.addEventListener('click', () => {
+    window.location.href = 'about.html';
+});
+
+portfolioLink.addEventListener('click', () => {
+    window.location.href = 'portfolio.html';
+});
+
+
+
+/*
 const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controlls");
 const sectBtn = document.querySelectorAll(".control");
@@ -31,10 +82,13 @@ function PageTransition(){
             const element = document.getElementById(id);
             //console.log(element);
             //console.log(element.classList);
+            
             element.classList.add('active');
+        
 
         }
     })
 }
 
 PageTransition();
+*/
